9ed00e4575890d8bd139b75ab9b4dcda5596c7dc
LailyAneTrihapsari <callmeleaaa99@gmail.com>
2025-12-15 10:58:26 +0700

    committed

--- pertemuan-1/praktikum_b/Pajak.php
+++ pertemuan-1/praktikum_b/Pajak.php
@@ -4,6 +4,14 @@ private float $tarif;
 public function __construct(float $tarif) {
 $this->tarif = $tarif;
 }
+
+
+
+
+
+
+
+
 public function hitungTotal(float $hargaProduk): float {
 return $hargaProduk + ($hargaProduk * $this->tarif);
 }
